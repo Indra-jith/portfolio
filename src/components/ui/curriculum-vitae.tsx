90e89871e046f75502e7791ea54735627e2e6303
indra-jith <indrajithmp77@gmail.com>
2026-02-12 06:42:09 +0530

    updated portfolio

--- src/components/ui/curriculum-vitae.tsx
+++ src/components/ui/curriculum-vitae.tsx
@@ -4,8 +4,8 @@ import { MdOutlineOpenInNew } from "react-icons/md";
 export const ViewCVButton = () => {
     const handleDownload = () => {
         const link = document.createElement('a');
-        link.href = '/IndrajithMP_Resume.pdf';
-        link.download = 'IndrajithMP_Resume.pdf';
+        link.href = '/Indrajith_Resume2026.pdf';
+        link.download = 'Indrajith_Resume2026.pdf';
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
